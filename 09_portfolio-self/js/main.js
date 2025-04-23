144351b6ad5694d2465d1ac599ab9c4402648395
eunbin <eunbin00@gmail.com>
2025-04-23 18:21:42 +0900

    up

--- 09_portfolio-self/js/main.js
+++ 09_portfolio-self/js/main.js
@@ -36,3 +36,42 @@ const swiper = new Swiper('.project .swiper', {
   },
 
 });
+
+
+
+
+
+
+
+
+
+// 현재 연도 표시
+// 날짜 정보를 가진 JS의 Date 객체를 활용
+// 알아서 현재 년도로 바꿔줌
+console.log(new Date().getFullYear());
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
+
+
+
+// 페이지를 최상단으로 이동
+const toTopEl = document.querySelector('#to-top');
+
+// 페이지에 스크롤 이벤트 감지를 추가!
+// window: 브라우저 창 객체
+window.addEventListener('scroll', function () {
+  // console.log(window.scrollY);  // y축 스크롤 위치
+  
+  // 페이지 스크롤 위치가
+  // 500px를 넘으면 요소를 보이고, 
+  // 500px를 넘지 않으면 요소 숨기기!
+  if (this.window.scrollY > 500) {
+    // 요소 보이기
+    toTopEl.style.opacity = '1';
+    toTopEl.style.transform = 'translateX(0)';
+  } else {
+    toTopEl.style.opacity = '0';
+    toTopEl.style.transform = 'translate(100px)';
+  }
+})
